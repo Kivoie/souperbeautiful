8471e38c6d064f5d231ce31cd8e3cf54f282ebf0
Kivoie <mindarc14@gmail.com>
2019-12-08 13:02:42 -0500

    Made some changes to the code

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ async def on_message(message):
 		#await message.channel.send(parserny)
 
 		article = 1		#start counter at 1
-		url = 'https://www.nytimes.com/'
-		#https://www.nytimes.com/2019/09/23/us/politics/trump-un-biden-ukraine.html?action=click&module=Top%20Stories&pgtype=Homepage
-		#https://www.bloomberg.com/news/articles/2019-08-16/cybersecurity-tips-from-a-master-of-deception-turned-consultant
+		url = 'https://www.bloomberg.com/news/'
+		#https://www.nytimes.com/
+		#https://www.bloomberg.com/
 
 		r = requests.get(url)	#get method to get url
 		r_html = r.text		#convert html into text
@@ -49,7 +49,7 @@ async def on_message(message):
 
 		for title in soup.find_all('article'):		#print out all the strings starting with the h2 tag
 			try:
-				print(await message.channel.send(title.find('p').text))	#print the article count number followed by the name of the headline
+				await message.channel.send(title.find('p').text)	#print the article count number followed by the name of the headline
 			except AttributeError:
 				break
 			article += 1		#increment counter by 1 for every loop
@@ -62,5 +62,10 @@ async def on_message(message):
 		await message.channel.send("lmao")
 	elif message.content == ("lmao"):
 		await message.channel.send("ayy")
+	#elif message.content == ("!soup help"):
+		#await message.client.user.send("Hello world")
+	elif message.content == ("!soup eat"):
+		await message.channel.send("Bye bye!")
+		await client.close()
 
 client.run(token)
