0fa80f9d3675a8c8ab9f7c076ff94e87a55b833b
kivoie <mindarc14@gmail.com>
2025-02-10 22:10:56 -0500

    added network check

--- souperbeautiful_daemon.sh
+++ souperbeautiful_daemon.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-sleep 10
+while :
+do
+
+	reachability=$(fping -t2000 8.8.8.8 -r 0)
+	if [ -n "$(echo $reachability | grep 'is alive')" ]
+	then
+		break
+	fi
+	sleep 5
+
+done
+
 $(date +"[%Y%m%d-%H:%M:%S] crons/boot/: started souperbeautiful" >> /home/ubuntu/Documents/my_debugs.log)
 /bin/python3 /home/ubuntu/Documents/souperbeautiful/main.py
 
